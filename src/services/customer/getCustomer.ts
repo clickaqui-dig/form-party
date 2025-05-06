5f69e9a5ea3a5b871f190fe94fb4c85f71ec32ad
Matheus Moraes de Faria <matheus.devjava@gmail.com>
2025-05-05 23:42:34 -0300

    .

--- src/services/customer/getCustomer.ts
+++ src/services/customer/getCustomer.ts
@@ -1,15 +1,8 @@
-<<<<<<< HEAD
-import { buildQueryParams } from '@/utils/builders/buildQueryParams';
-import api from '../../config/apiConfig';
-import { Customer } from '@/models/Customer';
-
-=======
 import { Customer } from "@/models/Customer";
 import { buildQueryParams } from "@/utils/builders/buildQueryParams";
 import api from '@/config/apiConfig';
 
 const URL = 'http://localhost:8080';
->>>>>>> 647d27afedf7d83900eb7a48e5fce2f80afd22fd
 
 interface RequestCustomer {
     page: number,
@@ -32,11 +25,7 @@ export const getCustomer = async(
     try {
         const query = buildQueryParams({ page, limit });
 
-<<<<<<< HEAD
-        const response = await api.get(`/cliente`);
-=======
         const response = await api.get(`${URL}/cliente`);
->>>>>>> 647d27afedf7d83900eb7a48e5fce2f80afd22fd
 
         return response.data;
     } catch (error) {
