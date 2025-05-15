8339ef320bbd5eb7564f22100068a183a4da023c
Matheus Moraes de Faria <matheus.devjava@gmail.com>
2025-05-15 00:10:46 -0300

    .

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,9 @@ import "./globals.css";
 import { SidebarProvider } from "@/context/SidebarContext";
 import { ThemeProvider } from "@/context/ThemeContext";
 
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+
 const outfit = Outfit({
   variable: "--font-outfit-sans",
   subsets: ["latin"],
@@ -18,6 +21,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={`${outfit.variable} dark:bg-gray-900`}>
         <ThemeProvider>
+          <ToastContainer />
           <SidebarProvider>{children}</SidebarProvider>
         </ThemeProvider>
       </body>
