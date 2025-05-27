346696879ddc1a13cacee97ac9df5743a98ef3c5
João Victor Tiburcio <jvictor.jvt@gmail.com>
2025-05-27 00:54:13 -0300

    feat: fix ngrok

--- src/config/apiConfig.ts
+++ src/config/apiConfig.ts
@@ -4,6 +4,9 @@ import Router from 'next/router';
 
 const api = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
+    headers: {
+        'ngrok-skip-browser-warning': 'true'
+    }
 })
 
 api.interceptors.request.use(
